2860b76f780481558d341ad2704a0faf5c39c66f
Abraham Jose <abrahamjose002@gmail.com>
2024-10-02 11:12:46 +0530

    Final changes added

--- src/config/rabbitMQ.config.ts
+++ src/config/rabbitMQ.config.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 export default {
     rabbitMQ:{
-        url:String(process.env.RabbitMQ_link),
+        url:String(process.env.RabbitMQ_Link),
         queues:{
             instructorQueue:'instructor_queue',
             
